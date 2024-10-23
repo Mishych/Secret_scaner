46a2c09b147e4af5e1bd97ad14421a6c1916a9a0
Mishych <mtcebak@gmail.com>
2024-10-23 19:36:58 +0300

    Edited find_commit_secrets and parse_arguments function

--- scan_commits.py
+++ scan_commits.py
@@ -6,13 +6,11 @@ import argparse
 
 
 # Gets commits from a GitHub repository using GitHub API
-def get_commits(owner, repo, token):
-    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-    
+def get_commits(repo, token):
+    url = f"https://api.github.com/repos/{repo}/commits"
     headers = {
         'Authorization': f'token {token}'
     }
-    
     response = requests.get(url, headers=headers)
     
     if response.status_code == 200:
@@ -25,30 +23,26 @@ def get_commits(owner, repo, token):
 # Search secrets in commit messages
 def find_commit_secrets(t_regexp, commits):
     output = ""
-    match_count = {}
+    
     try:
-        for regexp in t_regexp:
-            for commit in commits:
+        for commit in commits:
+            highlighted_line = commit
+            found_match = False
+            for regexp in t_regexp:
                 matches = re.findall(regexp, commit)
+                
                 if matches:
+                    found_match = True
                     for match in matches:
-                        match_key = match[1]
-
-                        if match_key in match_count:
-                            match_count[match_key] += 1
-                        else:
-                            match_count[match_key] = 1
-
-                        if match_count[match_key] > 3:
-                            continue
+                        highlighted_match = f"{fg('light_green')}{match}{attr(0)}"
+                        highlighted_line = highlighted_line.replace(match, highlighted_match)
                         
-                        output += f"\n>>> Found secrets in commit\n"
-                        output += f"{fg('white')}{match[0].lstrip()}{attr(0)}" + \
-                                    f"{fg('light_green')}{match[1]}{attr(0)}" + \
-                                    f"{fg('white')}{match[-1].rstrip()}{attr(0)}\n"
+            if found_match:           
+                output += f"\n>>> Found secrets in commit\n"
+                output += highlighted_line
         
-            if not output:
-                print(f"[+] No secrets found in commits")
+        if not output:
+            print(f"[+] No secrets found in commits")
 
     except Exception as e:
         error_message = f"[-] Error occurred while reading : {e}"
@@ -63,10 +57,8 @@ parser = argparse.ArgumentParser()
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--token", required=True, help="GitHub token for authentication")
-    parser.add_argument("-o", "--owner", required=True, help="Repository owner")
-    parser.add_argument("-r", "--repo", required=True, help="Repository name")
+    parser.add_argument("-r", "--repo", required=True, help="Repository path")
     parser.add_argument("-re", "--regex", nargs="+", default=None, help="One or more regular expressions to search for secrets (optional)")
-    
     return parser.parse_args()
 
 # Main function
@@ -74,28 +66,26 @@ def main():
     args = parse_arguments()
       
     if args.regex:
-        t_regexp = [re.compile(r'(.{0,100})(' + reg + ')(.{0,100})', re.IGNORECASE) for reg in args.regex]
+        t_regexp = [re.compile(reg, re.IGNORECASE) for reg in args.regex]
         
     else:
-        regexp_to_search = "regex_patterns/regex_secrets.csv" 
+        regexp_to_search = "regex_secrets.csv" 
         if os.path.exists(regexp_to_search):
             with open(regexp_to_search, "r", encoding="UTF-8") as file_regs:
                 regs = file_regs.readlines()
                 t_regexp = [reg.strip() for reg in regs if reg.strip()]
-                t_regexp = [re.compile(r'(.{0,100})(' + regexp + ')(.{0,100})', re.IGNORECASE) for regexp in t_regexp]
+                t_regexp = [re.compile(regexp, re.IGNORECASE) for regexp in t_regexp]
         else:
             print(f"{fg('yellow')}[-] Search term is missing.{attr(0)}")
             exit(1)
             
     commits_list = []
-    
-    commits = get_commits(args.owner, args.repo, args.token)
+    commits = get_commits(args.repo, args.token)
 
     if commits:
         for commit in commits:
             commits_list.append(commit['commit']['message'])
             
-        
     output = []
     results = find_commit_secrets(t_regexp, commits_list)
     if results:
@@ -103,7 +93,6 @@ def main():
 
     if output:
         print("\n".join(output))
-        
     else:
         print(f"{fg('yellow')}[-] No secrets found.{attr(0)}")
     
